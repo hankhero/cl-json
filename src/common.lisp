ae333012dabc386a49ab3f101389ecbb90976e38
Kieran Grant <kieran.thehacker.grant@gmail.com>
2017-06-29 10:56:16 +1000

    Add a way to force 'false' boolean output when encoding JSON

--- src/common.lisp
+++ src/common.lisp
@@ -71,10 +71,13 @@ slash.")
 
 ;;; Symbols
 
+(defconstant +JSON-FALSE+ '+JSON-FALSE+ "Special Constant to force the output of 'false' in an encoded JSON structure. Some applications are known to test for a value of 'false', meaning that a value of 'null' is not enough, this value can be used for those special occasions.")
+
 (defparameter +json-lisp-symbol-tokens+
   '(("true" . t)
     ("null" . nil)
-    ("false" . nil))
+    ("false" . nil)
+    ("false" . +JSON-FALSE+))
   "Mapping between JSON literal names and Lisp boolean values.")
 
 (defvar *json-symbols-package* (find-package 'keyword)
